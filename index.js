6af9214f7f4ca9d607b5baa75ab9bd0d7c9ff61f
golden-state0fmind <areyes2009@yahoo.com>
2021-02-28 14:29:15 -0700

    modular repair routes with api

--- index.js
+++ index.js
@@ -10,23 +10,19 @@ const port = process.env.PORT || 3000
 app.set('view engine', 'ejs')
 //Middleware
 app.use(require('morgan')('dev'))
+app.use(express.urlencoded({extended:false}))
 app.use(ejsLayouts)
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public/'))
 
 
 app.get('/', function (req, res) {
-    // let vin= '1NXBR30E56Z572979'
-    // let mileage = 175000
-    // let baseURL = `http://api.carmd.com/v3.0/decode?vin=${vin}`;
-    // axios.get(baseURL).then(function (apiResponse){
-    //     let carInfo = apiResponse.data.results
-    //     console.log(carInfo)
-    // })
     res.render('index')
 })
 
 //Import all routes from the routes directory
 app.use('/repair',require('./routes/repair'))
+
+//Server
 let server = app.listen(port, function (){
     console.log('... listening on', port)
 })
