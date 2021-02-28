0d624ce2732554c264e044328c3a3a679150c50c
golden-state0fmind <areyes2009@yahoo.com>
2021-02-28 14:31:31 -0700

    modifies route to work and render repair page

--- routes/repair.js
+++ routes/repair.js
@@ -3,8 +3,21 @@ const db=null
 const router = express.Router()
 const axios=require('axios')
 
-router.get('/'), function(req,res){
-    res.send('Repair Page')
-}
+router.get('/', function(req,res){
+    // let vin= '1NXBR30E56Z572979'
+    // let mileage = 175000
+    // let baseURL = `http://api.carmd.com/v3.0/decode?vin=${vin}`;
+    // axios.get(baseURL).then(function (apiResponse){
+    //     let carInfo = apiResponse.data.results
+    //     console.log(carInfo)
+    // })
+    // res.render('repair',{carInfo:carInfo})
+    res.send('hello from repair shops')
+})
+
+
+router.post('/', function(req,res){
+    res.redirect('/repair')
+})
 
 module.exports=router
