c4d262e6cc968cbdb593d13716d7a1c7c28efd3c
golden-state0fmind <areyes2009@yahoo.com>
2021-02-27 23:48:55 -0700

    adds route to repair page

--- index.js
+++ index.js
@@ -15,15 +15,18 @@ app.use(express.static(__dirname + '/public'))
 
 
 app.get('/', function (req, res) {
-    let vin= '1NXBR30E56Z572979'
-    let mileage = 175000
-    let baseURL = `http://api.carmd.com/v3.0/decode?vin=${vin}`;
-    axios.get(baseURL).then(function (apiResponse){
-        let carInfo = apiResponse.data.results
-        console.log(carInfo)
-    })
+    // let vin= '1NXBR30E56Z572979'
+    // let mileage = 175000
+    // let baseURL = `http://api.carmd.com/v3.0/decode?vin=${vin}`;
+    // axios.get(baseURL).then(function (apiResponse){
+    //     let carInfo = apiResponse.data.results
+    //     console.log(carInfo)
+    // })
+    res.render('index')
 })
 
+//Import all routes from the routes directory
+app.use('/repair',require('./routes/repair'))
 let server = app.listen(port, function (){
     console.log('... listening on', port)
 })
