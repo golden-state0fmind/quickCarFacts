9eb8a736f4cab9f8959f2beeae05b99ee893a1c2
golden-state0fmind <areyes2009@yahoo.com>
2021-03-02 22:49:21 -0700

    axios

--- routes/repair.js
+++ routes/repair.js
@@ -4,14 +4,14 @@ const router = express.Router()
 const axios=require('axios')
 
 router.get('/', function(req,res){
-    // let vin= '1NXBR30E56Z572979'
-    // let mileage = 175000
-    // let baseURL = `http://api.carmd.com/v3.0/decode?vin=${vin}`;
-    // axios.get(baseURL).then(function (apiResponse){
-    //     let carInfo = apiResponse.data.results
-    //     console.log(carInfo)
-    // })
-    // res.render('repair',{carInfo:carInfo})
+    let vin= '1NXBR30E56Z572979'
+    let mileage = 175000
+    let baseURL = `http://api.carmd.com/v3.0/decode?vin=${vin}`;
+    axios.get(baseURL).then(function (apiResponse){
+        let carInfo = apiResponse.data.results
+        console.log(carInfo)
+    })
+    res.render('repair',{carInfo:carInfo})
     res.send('hello from repair shops')
 })
 
