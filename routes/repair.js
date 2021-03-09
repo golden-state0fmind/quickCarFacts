221c7d630e5022256f7444012ead9afe868bf75f
golden-state0fmind <areyes2009@yahoo.com>
2021-03-08 23:46:06 -0700

    cleans up route

--- routes/repair.js
+++ routes/repair.js
@@ -1,29 +1,29 @@
+require ('dotenv').config();
 const express=require('express')
 const db=null
 const router = express.Router()
 const axios=require('axios')
-const dotenv =require('dotenv')
-dotenv.config()
+
 //Local Variables 
-let vin= '1NXBR30E56Z572979'
-let mileage = 175000
-let baseURL = `http://api.carmd.com/v3.0/upcoming?vin=${vin}&mileage=${mileage}`;
-//Gets car info based on the VIN number of the vehicle
-//Info retrieved is upcoming mechanic work
-axios.get(baseURL, { 
-    headers: { 
-        'Authorization': process.env.API_KEY,
-        'partner-token':process.env.PARTNER_TOKEN
-    }})
-.then(response => {
-    console.log(response.data);
-})
-.catch((error) => {
-    console.log('error ' + error);
-});
-//
-router.post('/', function(req,res){
-    res.redirect('/repair')
+// let vin= '1NXBR30E56Z572979'
+// let mileage = 175000
+// let baseURL = `http://api.carmd.com/v3.0/decode?vin=${vin}&mileage=${mileage}`;
+// //Gets car info based on the VIN number of the vehicle
+// //Info retrieved is upcoming mechanic work
+// axios.get(baseURL, { 
+//     headers: { 
+//         'Authorization': process.env.API_KEY,
+//         'partner-token':process.env.PARTNER_TOKEN
+//     }})
+// .then(response => {
+//     console.log(response.data);
+// })
+// .catch((error) => {
+//     console.log('error ' + error);
+// });
+
+router.get('/', function(req,res){
+    res.render('repair')
 })
 
 module.exports=router
