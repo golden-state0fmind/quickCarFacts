0517262f23a9a78d635a46d0f4b27f5b44d2d642
golden-state0fmind <areyes2009@yahoo.com>
2021-03-08 22:45:39 -0700

    cleans up code and GET's upcoming maintenance for vehicle

--- routes/repair.js
+++ routes/repair.js
@@ -4,20 +4,13 @@ const router = express.Router()
 const axios=require('axios')
 const dotenv =require('dotenv')
 dotenv.config()
-
-// router.get('/', function(req,res){
-//     let vin= '1NXBR30E56Z572979'
-//     let mileage = 175000
-//     let baseURL = `http://api.carmd.com/v3.0/decode?vin=${vin}`;
-//     axios.get(baseURL).then(function (apiResponse){
-//         let carInfo = apiResponse.data.results
-//         console.log(carInfo)
-//     })
-//     res.render('repair',{carInfo:carInfo})
-//     res.send('hello from repair shops')
-// })
-
-axios.get('http://api.carmd.com/v3.0/decode?vin=1NXBR30E56Z572979', { 
+//Local Variables 
+let vin= '1NXBR30E56Z572979'
+let mileage = 175000
+let baseURL = `http://api.carmd.com/v3.0/upcoming?vin=${vin}&mileage=${mileage}`;
+//Gets car info based on the VIN number of the vehicle
+//Info retrieved is upcoming mechanic work
+axios.get(baseURL, { 
     headers: { 
         'Authorization': process.env.API_KEY,
         'partner-token':process.env.PARTNER_TOKEN
@@ -28,7 +21,7 @@ axios.get('http://api.carmd.com/v3.0/decode?vin=1NXBR30E56Z572979', {
 .catch((error) => {
     console.log('error ' + error);
 });
-
+//
 router.post('/', function(req,res){
     res.redirect('/repair')
 })
