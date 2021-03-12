57fd5a5b19e0df2a171af32f4e7bcb78450808aa
golden-state0fmind <areyes2009@yahoo.com>
2021-03-11 21:30:36 -0700

    gets vin and mileage from URL and fetches data from API

--- routes/repair.js
+++ routes/repair.js
@@ -4,25 +4,24 @@ const db=null
 const router = express.Router()
 const axios=require('axios')
 
-//Local Variables 
-// let vin= '1NXBR30E56Z572979'
-// let mileage = 175000
-// let baseURL = `http://api.carmd.com/v3.0/decode?vin=${vin}&mileage=${mileage}`;
-// //Gets car info based on the VIN number of the vehicle
-// //Info retrieved is upcoming mechanic work
-// axios.get(baseURL, { 
-//     headers: { 
-//         'Authorization': process.env.API_KEY,
-//         'partner-token':process.env.PARTNER_TOKEN
-//     }})
-// .then(response => {
-//     console.log(response.data);
-// })
-// .catch((error) => {
-//     console.log('error ' + error);
-// });
-
+//Repair page
 router.get('/', function(req,res){
+    const vin = req.query.vin;
+    const mileage = req.query.mileage;
+    //Gets car info based on the VIN number of the vehicle
+    //Info retrieved is upcoming mechanic work
+    let baseURL = `http://api.carmd.com/v3.0/decode?vin=${vin}&mileage=${mileage}`;
+    axios.get(baseURL, { 
+        headers: { 
+            'Authorization': process.env.API_KEY,
+            'partner-token': process.env.PARTNER_TOKEN
+        }})
+    .then( response => {
+        console.log(response.data)
+    })
+    .catch((error) => {
+        console.log('error ' + error);
+    });
     res.render('repair')
 })
 
